9fc3414d8caa93339733b3f19187c4358cf5e421
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-06-26 05:59:23 +0530

    sigin and signup created

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = 4000
+const mongoose = require('mongoose')
+const User = require('./models/user')
+const router = require('./routes/auth')
+
+////
+const uri = `mongodb+srv://${process.env.MUser}:${process.env.MPassword}@cluster0-on2ha.mongodb.net/<dbname>?retryWrites=true&w=majority`
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology:true})
+    .then(()=>{
+        console.log("Mongo Connected")
+    })
+    .catch(err => {
+        console.log(err)
+    })
+
+app.use(express.json())
+
+//routes
+app.use('/',router)
+
+app.listen(PORT, ()=> {
+    console.log(`Server started on port ${PORT}`)
+})
---
+++ routes/auth.js
@@ -0,0 +1,89 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+
+router.get('/',(req,res)=>{
+    res.send('hello aslkdla')
+})
+
+router.post('/signup',(req,res)=>{
+    // console.log(req.body)
+    const {name, email, password} = req.body
+    if(!email || !name || !password){
+        return res.status(422).json({message:{
+            msg: "One or more fields are empty. Fill all the details",
+            err: true
+        }})
+    }else{
+        User.findOne({email})
+            .then(user =>{
+                if(user){
+                    res.status(422).json({message:{
+                        msg: "User already exits",
+                        err: true
+                    }})             
+                }else{
+                    bcrypt.hash(password,12)
+                    .then(hashedPassword => {
+                        const newUser = new User({name,email,password: hashedPassword})
+                        newUser.save()
+                        .then(user => {
+                            res.status(200).json({message:{
+                                msg: "User Successfuly created",
+                                err: false
+                            }})                     
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            throw err
+                        })
+                    })
+                }
+            })
+            .catch(err => {
+                console.log(err)
+                throw err
+            })
+    }
+})
+
+router.post('/signin',(req,res)=>{
+    const {email, password} = req.body
+    if(!email || !password){
+        return res.status(422).json({message: {
+            msg: "One or more fields are empty",
+            err: true
+        }})
+    }else{
+        User.findOne({email})
+        .then(user => {
+            if(!user){
+                return res.status(422).json({message:{
+                    msg: "Invalid email or password",
+                    err: true
+                }})
+            }else{
+                bcrypt.compare(password,user.password)
+                .then(ifmatch => {
+                    if(ifmatch){
+                        res.status(200).json({message: {
+                            msg: "Success",
+                            err: false
+                        }})
+                    }else{
+                        return res.status(422).json({message: {
+                            msg: "Invalid email or password",
+                            err: true
+                        }})
+                    }
+                })
+                .catch(err => {
+                    console.log(err)
+                    throw err
+                })
+            }
+        })
+    }
+})
+module.exports = router
