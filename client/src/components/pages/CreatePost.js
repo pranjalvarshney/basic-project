a4a69c864eb8b21727b55925fa3f2a961f2ea6c3
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-06-27 23:47:14 +0530

    form fixed

--- client/src/components/pages/CreatePost.js
+++ client/src/components/pages/CreatePost.js
@@ -6,12 +6,15 @@ const Createpost = () =>{
         <div className="CreatePost">
             <Card className="create-post">
                 <h3>Create Post</h3>
-                <input className="title-input" placeholder="title"/>
-                <input className="caption-input" placeholder="caption"/>
-                <div class="custom-file">
-                    <input type="file" class="custom-file-input" id="customFile" />
-                    <label class="custom-file-label" for="customFile">Select Image</label>
-                </div>
+                <form>
+                    <input className="title-input" placeholder="title"/>
+                    <input className="caption-input" placeholder="caption"/>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFile" />
+                        <label class="custom-file-label" for="customFile">Select Image</label>
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit post</button>
+                </form>
             </Card>
         </div>
     )
