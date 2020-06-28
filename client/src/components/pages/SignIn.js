7dbee9e62323cf9db36c3d5378eb324f833ba722
Pranjal Varshney <pranjal25varshney@gmail.com>
2020-06-29 03:08:21 +0530

    signin post added

--- client/src/components/pages/SignIn.js
+++ client/src/components/pages/SignIn.js
@@ -1,27 +1,98 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { Card, Form, Button } from 'react-bootstrap'
-import { Link } from "react-router-dom"
+import { Link,useHistory  } from "react-router-dom"
+import axios from 'axios'
 
 const SignIn = () =>{
+
+    const history = useHistory()
+    const [values, setValues] = useState({email: "",password: ""})
+    const [error , setError] = useState({status: false, msg: "", color: ""})
+
+    const handleChange = (e) => {
+        const {name, value} = e.target
+        setError({
+            status: false,
+            msg: "",
+            color: ""
+        })
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    const formData = {
+        email: values.email,
+        password: values.password
+    }
+    const uri = "http://localhost:4000/signin"
+    const PostSigninData = (e) => {
+        e.preventDefault()
+        if(values.email || values.password){
+            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)){
+                if(values.password > 5){
+                    axios.post(uri,formData)
+                    .then(response=> {
+                        console.log(response)
+                        setError({
+                            status: false,
+                            msg: response.data.message.msg,
+                            color: "success"
+                        })
+                        history.push("/")
+                    })
+                    .catch(err=> {
+                        // console.log(err.response)
+                        setError({
+                            status: true,
+                            msg: err.response.data.message.msg,
+                            color: "danger"
+                        })
+                    })
+                }else{
+                    setError({
+                        status: true,
+                        msg: "Password length should be greater than 5",
+                        color: "danger"
+                    })
+                }
+            }else{
+                setError({
+                    status: true,
+                    msg: "Invalid e-mail",
+                    color: "danger"
+                })
+            }
+        }else{
+            setError({
+                status: true,
+                msg: "One or more fields are empty",
+                color: "danger"
+            })
+        }
+    }
+
     return (
         <div className="Signin">
             <Card style={{width: "50%"}} className=" mx-auto my-5 py-5 px-3">
-                <div class="container">
+                <div className="container">
                     <h2 className="text-center">Signin</h2>
-                    <Form>     
+                    <Form onSubmit={e=>PostSigninData(e)}>     
                         <Form.Group controlId="formBasicEmail">
                             <Form.Label>Email address</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control type="email" name="email" placeholder="Enter email" onChange={handleChange} value={values.email} />
                         </Form.Group>
 
                         <Form.Group controlId="formBasicPassword">
                             <Form.Label>Password</Form.Label>
-                            <Form.Control type="password" placeholder="Password" />
+                            <Form.Control type="password" name="password" placeholder="Password" onChange={handleChange} value={values.password} />
                         </Form.Group>
                         <div className="submit-div">
                             <Button variant="primary" type="submit">Submit</Button>
                             <Link to="/signup">New? Create an account</Link>
-                        </div>                       
+                        </div>
+                        <h6 className={`mt-3 text-center text-${error.color}`}>{error.status ? error.msg : null}</h6>                       
                     </Form>
                 </div>
             </Card>
--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react'
 import { Form , Button, Card } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import axios from 'axios'
 
 const SignUp = () =>{
@@ -8,6 +8,7 @@ const SignUp = () =>{
     const [values, setValues] = useState({pname:"", email: "", password:""})
     const [error, setError] = useState({status: false, msg: "",color: ""})
 
+    const history = useHistory()
     const handleChange = e => {
         const {name, value} = e.target
         setError({status:false, msg:"",color:""})
@@ -27,25 +28,50 @@ const SignUp = () =>{
     const uri="http://localhost:4000/signup"
     const PostSignupData = async (e) =>{
         e.preventDefault()
-        axios.post(uri,formData)
-        .then(response=> {
-            console.log(response)
-            setError({
-                status: false,
-                msg: response.data.message.msg,
-                color: "success"
-            })
-        })
-        .catch(err =>{
-            console.log(err.response.data)
-            if(err.response.data.message.err){
+        if(values.name || values.email || values.password){
+            if(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)){
+                if(values.password.length > 5){
+                    axios.post(uri,formData)
+                    .then(response=> {
+                        console.log(response)
+                        setError({
+                            status: false,
+                            msg: response.data.message.msg,
+                            color: "success"
+                        })
+                        history.push("/signin")
+                    })
+                    .catch(err =>{
+                        console.log(err.response.data)
+                        if(err.response.data.message.err){
+                            setError({
+                                status:true,
+                                msg:err.response.data.message.msg,
+                                color: "danger"
+                            })
+                        }
+                    })
+                }else{
+                    setError({
+                        status: true,
+                        msg: "Password length should be greater than 5",
+                        color: "danger"
+                    })    
+                }
+            }else{
                 setError({
-                    status:true,
-                    msg:err.response.data.message.msg,
+                    status: true,
+                    msg: "Invalid e-mail",
                     color: "danger"
                 })
             }
-        })
+        }else{
+            setError({
+                status: true,
+                msg: "One or more fields are empty",
+                color: "danger"
+            })
+        }
     }
 
     return (
@@ -53,7 +79,7 @@ const SignUp = () =>{
             <Card style={{width: "50%"}} className=" mx-auto my-5 py-5 px-3">
                 <div className="container">
                     <h2 className="text-center">Signup</h2>
-                    <Form onSubmit={(e)=>PostSignupData(e)}>
+                    <Form onSubmit={(e)=>PostSignupData(e)} noValidate>
                         <Form.Group controlId="formBasicName">
                             <Form.Label>Name</Form.Label>
                             <Form.Control type="text" name="pname" placeholder="Full name" value={values.pname} onChange={handleChange} />
@@ -68,7 +94,7 @@ const SignUp = () =>{
                             <Form.Label>Password</Form.Label>
                             <Form.Control type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange}/>
                         </Form.Group>
-                        <h6 class={`text-center text-${error.color}`}>{error.status ? error.msg : error.msg}</h6>
+                        <h6 className={`text-center text-${error.color}`}>{error.status ? error.msg : error.msg}</h6>
                         <div className="submit-div">
                             <Button variant="primary" type="submit">Submit</Button>
                             <Link to="/signin">Already an user? Signin </Link>
